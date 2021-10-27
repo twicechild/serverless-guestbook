c9a7be33abbe9127c0ed39e168524236e8bd828e
twicechild <73764054+twicechild@users.noreply.github.com>
2021-10-27 18:28:47 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://afd9ee7b.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
